869492783eafd5b69e6786804a15edac722b9a36
Hasan Bakhtiar <hasan.doctype@gmail.com>
2026-05-08 20:18:06 +0400

    lastest

---
+++ lesson1-introduction/js/script.js
@@ -0,0 +1,45 @@
+// console.log('Hello World');
+// Single line
+/**
+ * more line
+ */
+
+// Primitive DT (string,number,boolean)
+// string
+// var infoOne = "Hello World";
+// console.log(infoOne);
+
+// number
+// var infoTwo = 5;
+// console.log(infoTwo);
+
+// boolean
+// var infoThree = true;
+// console.log(infoThree);
+
+
+
+// Compiled DT
+// ARRAY
+// var infoFour = [1,"Hello",true,"Hello",[2,3,4,5]];
+// console.log(infoFour);
+
+
+// Object
+// var infoFive = {
+//     title:"Asus Zenbook",
+//     price:3000,
+//     newDevice:true,
+// }
+// console.log(infoFive);
+
+
+
+
+
+// Special DT
+// undefined
+// var a = null;
+// console.log(a);
+
+// null
