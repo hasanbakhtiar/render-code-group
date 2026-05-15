da5f39e7232aa63c24378128bb8716c6786d6f86
Hasan Bakhtiar <hasan.doctype@gmail.com>
2026-05-15 20:43:03 +0400

    lastest

---
+++ JavaScript/lesson3-conditions-loops/js/script.js
@@ -0,0 +1,82 @@
+// if = eger
+// else = deyilse
+// else if  = deyilse eger
+
+// let age = 2;
+// if (age>0 && age<=2) {
+//   console.log("Baby");
+// } else if (info == 10) {
+//   console.log("This is ten");
+// } else if (info == 7) {
+//   console.log("This is seven");
+// } else {
+//   console.log("Not found");
+// }
+
+// let messageCount = 5;
+
+// if (messageCount == 5) {
+//   messageCount = 4;
+//   console.log(" 5 message");
+//   if (messageCount == 4) {
+//     console.log("5-1 message");
+//   }
+// } else {
+//   console.log("0 message");
+// }
+
+// let info = 5;
+
+// if (info == 5) {
+//   console.log("This is 5");
+//   info = 4;
+//   switch (info) {
+//     case 4:
+//       console.log("This is 4");
+
+//       break;
+
+//     default:
+//       console.log("This is default");
+
+//       break;
+//   }
+// }
+
+// switch (info) {
+//   case 5:
+//     console.log("This is 5");
+//     info = 4;
+//     if (info == 4) {
+//       console.log("This is 4");
+//     }
+//     break;
+//   case 15:
+//     console.log("This is 15");
+//     break;
+//   default:
+//     console.log("This is default");
+//     break;
+// }
+
+// const productOne = ['bread','water','oil']; //10
+// const productTwo = ['onion','milk','egg']; //20
+// const productThree = ['meat','cola','fruits','fish']; //40
+// const productFour = productOne + productTwo; //70
+// const productFive = productTwo+ productThree //120
+
+// console.log(productFive);
+
+// try {
+//   let info = 5;
+//   if (info == 5) {
+//     console.log(5);
+//   }
+// } catch (error) {
+//    console.log("Burada nese problem var: " + error);
+//   console.log(error);
+// }finally{
+//     console.log("Men her halda");
+
+// }
+
