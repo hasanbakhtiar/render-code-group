aa42b85e7539bb8639265b783df22b94d1c72cea
Hasan Bakhtiar <hasan.doctype@gmail.com>
2026-06-01 18:56:30 +0400

    lastest

---
+++ JavaScript/lesson7-DOM-part1/js/script.js
@@ -0,0 +1,34 @@
+// const changeText = () => {
+//   document.getElementsByTagName("h1")[0].innerHTML = "New Value";
+// };
+
+// document.getElementsByTagName("button")[0].addEventListener("click",changeText);
+// document.getElementsByTagName("button")[0].onclick = changeText;
+
+// document.getElementById('start').innerHTML = "new value";
+// document.getElementsByClassName('info')[1].innerHTML = "new value 1";
+// document.getElementsByClassName('info')[1].innerText = "<i>new value 1</i>";
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByName
+// document.getElementsByTagName
+// document.getElementsByTagNameNS
+// document.querySelector
+// document.querySelectorAll
+
+// document.querySelectorAll("h1")[0].textContent = "new value";
+
+// const headText = document.querySelector("h1");
+// const btn = document.querySelector("button");
+
+// const changeData = () => {
+//   if (headText.textContent == "England") {
+//     headText.textContent = "London";
+//     btn.textContent = "Country";
+//   } else {
+//     headText.textContent = "England";
+//     btn.textContent = "Capital";
+//   }
+// };
+
+// btn.onclick = changeData;
