97d83341f621e90553b6a296319e53a30675a8ce
Hasan Bakhtiar <hasan.doctype@gmail.com>
2026-06-30 15:38:11 +0400

    reactjs start

---
+++ ReactJS/lesson1-introduction/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+
+}
