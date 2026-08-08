166ae5e94a7c49ee12def6582193bf744f93a347
hasanbakhtiar <hasan.doctype@gmail.com>
2026-08-08 19:27:12 +0400

    sass

---
+++ ReactJS/lesson12-sass/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+        <li><a href="">Test text</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
